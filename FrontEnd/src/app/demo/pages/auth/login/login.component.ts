e4a585c3d19e1399ea0941ef785caacc1656bb48
gabriel9107 <gabriel9107@gmail.com>
2025-03-05 15:54:26 -0400

    login

--- FrontEnd/src/app/demo/pages/auth/login/login.component.ts
+++ FrontEnd/src/app/demo/pages/auth/login/login.component.ts
@@ -1,6 +1,6 @@
 // angular import
-import { Component } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
+import { Component, inject, signal } from '@angular/core';
+import { FormBuilder, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 // project import
@@ -8,24 +8,55 @@ import { SharedModule } from 'src/app/demo/shared/shared.module';
 
 @Component({
   selector: 'app-login',
-  imports: [SharedModule, RouterModule],
+  imports: [SharedModule, RouterModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss', '../authentication.scss']
 })
 export default class LoginComponent {
   // public props
   hide = true;
-  email = new FormControl('', [Validators.required, Validators.email]);
-  Email = 'info@phoenixcoded.co';
-  password = '123456';
+  // email = new FormControl('', [Validators.required, Validators.email]);
+  // Email = 'info@phoenixcoded.co';
+  // password = '123456';
 
-  // public method
+
+fb = inject(FormBuilder) 
+
+hasError = signal(false) 
+isPosting = signal(false)
+
+  
+ loginForm = this.fb.group( {
+  email : ['', [Validators.required, Validators.email]], 
+  password : ['', [Validators.required, Validators.maxLength(6)]], 
+
+ });
+
+ onSumit(){
+  if(this.loginForm.invalid){
+    this.hasError.set(true); 
+    setTimeout(() => {
+      this.hasError.set(false);
+    }, 2000);
+    return;
+  }
+
+  const {email, password } = this.loginForm.value; 
+  console.log(email, password)
+ }
+
+
+
+
+
+
+// public method
   getErrorMessage() {
-    if (this.email.hasError('required')) {
-      return 'You must enter an email';
-    }
+    // if (this.email.hasError('required')) {
+    //   return 'You must enter an email';
+    // }
 
-    return this.email.hasError('email') ? 'Not a valid email' : '';
+    // return this.email.hasError('email') ? 'Not a valid email' : '';
   }
   loginType = [
     {
